41f2488a174d15150ece357ccb4ee8ad5e21910b
Cesar Augusto <augustocsar22@gmail.com>
2023-11-19 18:29:45 -0300

    deletando repositorios

--- src/paginas/InfoRepositorio/index.js
+++ src/paginas/InfoRepositorio/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import estilos from './estilos';
-import { salvarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { deletarRepositorioDoUsuario, salvarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
 
 export default function InfoRepositorio({ route, navigation }) {
     const [nome, setNome] = useState(route.params.item.name);
@@ -20,6 +20,19 @@ export default function InfoRepositorio({ route, navigation }) {
             navigation.goBack();
         } else {
             Alert.alert("Erro ao atualizar repositório");
+        }
+    }
+
+    async function deletar(params) {
+        const resultado = await deletarRepositorioDoUsuario(
+            route.params.item.id,
+        );
+
+        if (resultado == 'sucesso') {
+            Alert.alert("Repositorio deletado!");
+            navigation.goBack();
+        } else {
+            Alert.alert("Erro ao deletar repositório");
         }
     }
 
@@ -48,6 +61,7 @@ export default function InfoRepositorio({ route, navigation }) {
                 </Text>
             </TouchableOpacity>
             <TouchableOpacity 
+                onPress={deletar}
                 style={[estilos.botao, {backgroundColor: '#DD2B2B', marginTop: 10}]} 
             >
                 <Text style={estilos.textoBotao}>
